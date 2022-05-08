0d7e3bd761ed3ecb382a1220b3ff8e90f34598a0
taffyb <brecknockt@gmail.com>
2022-05-08 09:55:04 +0100

    fix addMove

--- lambda/sm_updateGameApplyMove/index.js
+++ lambda/sm_updateGameApplyMove/index.js
@@ -4,7 +4,7 @@ const docClient = new AWS.DynamoDB.DocumentClient();
 exports.handler = async (event, context, callback) => {
     const gameUuid=event.gameUuid;
     const moves=event.moves;
-    console.log(`Event:${JSON.stringify(event)}`);
+    //console.log(`Event:${JSON.stringify(event)}`);
     
     const TABLE_NAME = 'sm_games';
     const item = {
@@ -15,11 +15,11 @@ exports.handler = async (event, context, callback) => {
     };
     const game = await docClient.get(item).promise();
     const cards= game.Item.cards;
-    console.log(`Cards Before Moves:${JSON.stringify(cards)}`);
+    //console.log(`Cards Before Moves:${JSON.stringify(cards)}`);
     moves.forEach((m)=>{
         cards[m.to].push(cards[m.from].pop());
     });
-    console.log(`Cards After Moves:${JSON.stringify(cards)}`);
+    //console.log(`Cards After Moves:${JSON.stringify(cards)}`);
     var params = {
         TableName:"sm_games",
         Key:{
@@ -32,8 +32,33 @@ exports.handler = async (event, context, callback) => {
         },
         ReturnValues:"UPDATED_NEW"
     };
-    console.log(`Updating the item...\n${JSON.stringify(params)}`);
+    //console.log(`Updating the item...\n${JSON.stringify(params)}`);
     const result = await docClient.update(params).promise();
     
-    callback(null,result);
+    moves.forEach(async m => {
+        var params = {
+            TableName:"sm_moves",
+            Key:{'gameUuid': event.gameUuid, 'moveId':m.id},
+            UpdateExpression:`set  playerUuid=:p, #f=:f, #t=:t, isDisCard=:d, isUndo=:u, #ty=:ty`,
+            ExpressionAttributeValues:{
+                ":p":m.playerUuid,
+                ":f":m.from,
+                ":t":m.to,
+                ":d":m.isDiscard,
+                ":u":m.isUndo,
+                ":ty":m.type
+            },
+            ExpressionAttributeNames:{
+                "#t":"to",
+                "#f":"from",
+                "#ty":"type"
+            },
+            ReturnValues:"UPDATED_NEW"
+        };
+        console.log(`Update Params: ${JSON.stringify(params)}`);
+        const putMove = await docClient.update(params).promise();
+        
+    });
+    
+    callback(null,`sucess ${moves.length}`);
 };
